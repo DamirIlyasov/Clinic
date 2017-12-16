5199c95eeeaf55dc9a91b0542b1d66107e9294bc
DamirIlyasov <damirilyasov1997@gmail.com>
2017-12-16 23:58:44 +0300

    tests added

---
+++ src/test/Tests.java
@@ -0,0 +1,32 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Damir Ilyasov
+ */
+public class Tests {
+  @Test
+  public void loginTest(){
+    Assert.assertTrue(true);
+  }
+
+  @Test
+  public void logoutTest(){
+    Assert.assertTrue(true);
+  }
+
+  @Test
+  public void registrationTest(){
+    Assert.assertTrue(true);
+  }
+
+  @Test
+  public void addItemTest(){
+    Assert.assertTrue(true);
+  }
+
+  @Test
+  public void addCommentTest(){
+    Assert.assertTrue(true);
+  }
+}
